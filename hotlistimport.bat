4b212f19afdfa0fa092e062b4a20aa5c7ee1b5e4
Addison Klinke <addison@openalpr.com>
2019-08-20 16:26:52 -0600

    add Windows batch file template

---
+++ hotlistimport.bat
@@ -0,0 +1,3 @@
+set CONF_FILE="C:\path\to\openalpr_hotlist_import\config\my_config.yaml"
+"C:\ProgramData\Miniconda3\python.exe" "C:\path\to\openalpr_hotlist_import\hotlistimport.py" %1 %CONF_FILE%
+pause
